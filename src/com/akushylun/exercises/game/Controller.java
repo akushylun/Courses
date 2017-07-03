0d70a75ffebef64f4470fc2bce3fa1f2f7e4dc44
Andrii Kushylun <kushilun.andrey@gmail.com>
2017-07-03 23:31:57 +0300

    Refactor controller method by creating additional private method checkBoundaryValid(int res)

--- src/com/akushylun/exercises/game/Controller.java
+++ src/com/akushylun/exercises/game/Controller.java
@@ -3,44 +3,54 @@ package com.akushylun.exercises.game;
 import java.util.Scanner;
 
 public class Controller {
-	
-	private Model model;
-	
-	private View view;
-	
-	public Controller(Model model, View view) {
-		this.model = model;
-		this.view = view;
+
+    private Model model;
+
+    private View view;
+
+    public Controller(Model model, View view) {
+	this.model = model;
+	this.view = view;
+    }
+
+    public void processUser() {
+	Scanner sc = new Scanner(System.in);
+
+	model.setBoundaries(Model.DEFAULT_LOWER_BOUNDARY, Model.DEFAULT_UPPER_BOUNDARY);
+	model.rand();
+	System.out.println(model.getRandomSecretInBoundary());
+
+	while (!model.checkValue(inputIntValueWithScanner(sc))) {
 	}
-	
-	public void processUser() {
-		Scanner sc = new Scanner(System.in);
-		
-		model.setBoundaries(Model.DEFAULT_LOWER_BOUNDARY, Model.DEFAULT_UPPER_BOUNDARY);
-		model.rand();
-		System.out.println(model.getRandomSecretInBoundary());
-		
-		while (!model.checkValue(inputIntValueWithScanner(sc))) {}
-		
-		view.printCongratulation(model);
-		view.printWay(model);
+
+	view.printCongratulation(model);
+	view.printWay(model);
+    }
+
+    private int inputIntValueWithScanner(Scanner sc) {
+
+	int res = 0;
+	boolean findedNumber = false;
+	view.printMessage(view.getInputMessage(model.getLowerBoundary(), model.getUpperBoundary()));
+
+	while (findedNumber != true) {
+	    while (!sc.hasNextInt()) {
+		view.printWrongIntInput(model);
+		sc.next();
+	    }
+	    res = sc.nextInt();
+	    if (checkBoundaryValid(res) == true) {
+		findedNumber = true;
+	    }
 	}
-	
-	private int inputIntValueWithScanner(Scanner sc) {
-		int res = 0;
-		view.printMessage(view.getInputMessage(model.getLowerBoundary(), model.getUpperBoundary()));
-		
-		while (true) {
-			while (!sc.hasNextInt()) {
-				view.printWrongIntInput(model);
-				sc.next();
-			}
-			if ((res = sc.nextInt()) <= model.getLowerBoundary() || res >= model.getUpperBoundary()) {
-				view.printWrongRangeInput(model);
-				continue;
-			}
-			break;
-		}
-		return res;
-	}
+	return res;
+    }
+
+    private boolean checkBoundaryValid(int res) {
+	if (res <= model.getLowerBoundary() || res >= model.getUpperBoundary()) {
+	    view.printWrongRangeInput(model);
+	    return false;
+	} else
+	    return true;
+    }
 }
